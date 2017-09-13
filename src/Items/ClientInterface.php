b239ebf732c3a86ed5747fce98d0aaa13ed1221c
schwaen <s.muhle@plajo.de>
2017-09-13 15:22:17 +0200

    documentation Aiphilos\Api\Items\ClientInterface

--- src/Items/ClientInterface.php
+++ src/Items/ClientInterface.php
@@ -2,6 +2,7 @@
 namespace Aiphilos\Api\Items;
 
 use Aiphilos\Api\ClientInterface as GeneralClientInterface;
+use Aiphilos\Api\ContentTypesEnum;
 
 /**
  * search client interface
@@ -10,31 +11,90 @@ use Aiphilos\Api\ClientInterface as GeneralClientInterface;
 interface ClientInterface extends GeneralClientInterface
 {
     /**
-     * Search products
-     *
+     * Constructor
+     * 
      * @param string $db_name
+     */
+    public function __construct($db_name = null);
+    
+    /**
+     * Set the name
+     * 
+     * @param string $name
+     */
+    public function setName($name);
+    
+    /**
+     * Return the name
+     * 
+     * @return string|null
+     */
+    public function getName();
+    
+    /**
+     * Return the Database-Details
+     * 
+     * @return array
+     */
+    public function getDetails();
+    
+    /**
+     * Sets the scheme
+     * 
+     * @param array $scheme key -> value pairs @see ContentTypesEnum
+     */
+    public function setScheme(array $scheme);
+    
+    /**
+     * Deletes the database
+     */
+    public function delete();
+    
+    /**
+     * Adds an item
+     * 
+     * @param string|int $id
+     * @param array $item
+     */
+    public function addItem($id, array $item);
+    
+    /**
+     * Updates an item
+     * 
+     * @param string|int $id
+     * @param array $item
+     */
+    public function updateItem($id, array $item);
+    
+    /**
+     * Returns an item
+     * 
+     * @param string|int $id
+     */
+    public function getItem($id);
+    
+    /**
+     * Deletes an item
+     * 
+     * @param string|int $id
+     */
+    public function deleteItem($id);
+    
+    /**
+     * insert/update/delete items
+     * @param array $items
+     */
+    public function batchItems(array $items);
+    
+    /**
+     * Search items
+     *
      * @param string $string
      * @param string $language
      *
      * @return false|array
      */
     public function searchItems($string, $language = null);
-    
-    public function __construct($db_name = null);
-    
-    //Item Handling
-    public function addItem($id, array $item);
-    public function updateItem($id, array $item);
-    public function getItem($id);
-    public function deleteItem($id);
-    public function batchItems(array $items);
-    
-    //database
-    public function setName($name);
-    public function getName();
-    public function getDetails();
-    public function setScheme(array $scheme);
-    public function delete();
     
     //@todo list all databases
 }
