9bf8853f586efcf8f7f43e650bec2fcf7eea1ac9
schwaen <s.muhle@plajo.de>
2017-09-13 14:41:49 +0200

    Add ContentTypesEnum

---
+++ src/ContentTypesEnum.php
@@ -0,0 +1,16 @@
+<?php
+namespace Aiphilos\Api;
+
+class ContentTypesEnum
+{
+    const GENERAL_AUTO = 'general.auto';
+    const PRODUCT_NUMBER = 'product.number';
+    const PRODUCT_NAME = 'product.name';
+    const PRODUCT_DESCRIPTION = 'product.description';
+    const PRODUCT_PRICE = 'product.price';
+    const PRODUCT_GTIN = 'product.gtin';
+    const PRODUCT_MANUFACTURER = 'product.manufacturer';
+    const PRODUCT_RATIN = 'product.rating';
+    const PRODUCT_STATE = 'product.state'; //[new,used,refurbished]
+    const ORDER_FREQUENCY = 'order.frequency';
+}
